f95667b2af44ca90479e8a1a06ff69d0ece1a116
Blaszke Krzysztof <krzysztof.blaszke.ctr@sabre.com>
2017-12-27 13:44:54 +0100

    [feature/miniMaxSum] Mini Max Sum kata

---
+++ scalaKata/src/test/scala/pl/blaszak/kata/MiniMaxSumTest.scala
@@ -0,0 +1,12 @@
+package pl.blaszak.kata
+
+import org.scalatest.FunSuite
+
+class MiniMaxSumTest extends FunSuite {
+
+  test("MinMaxSum.scala") {
+    assert(MiniMaxSum.getSums(Array(1,2,3,4,5)) === "10 14")
+    assert(MiniMaxSum.getSums(Array(5, 5, 5, 5, 5)) === "20 20")
+  }
+
+}
