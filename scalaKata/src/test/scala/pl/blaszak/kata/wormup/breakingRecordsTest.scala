6cf51cbcec71cb6ef651492d30e8692c5008fdd3
Blaszke Krzysztof <krzysztof.blaszke.ctr@sabre.com>
2018-03-30 18:28:15 +0200

    [feature/breakingRecordsKata] version 2.0

--- scalaKata/src/test/scala/pl/blaszak/kata/wormup/breakingRecordsTest.scala
+++ scalaKata/src/test/scala/pl/blaszak/kata/wormup/breakingRecordsTest.scala
@@ -5,7 +5,11 @@ import org.scalatest.FunSuite
 class breakingRecordsTest extends FunSuite {
 
   test("testBreakingRecords") {
+    assert(breakingRecords.breakingRecords(Nil) === None)
+    assert(breakingRecords.breakingRecords(List()) === None)
+    assert(breakingRecords.breakingRecords(List(10)) === Some(0, 0))
     assert(breakingRecords.breakingRecords(List(10, 5, 20, 20, 4, 5, 2, 25, 1)) === Some((4, 2)))
+    assert(breakingRecords.breakingRecords(List(3, 4, 21, 36, 10, 28, 35, 5, 24, 42)) === Some((0, 4)))
   }
 
 }
